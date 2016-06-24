6fb9c654547c4561880f51dcafdd0646ddbd961e
Gabriel Ramirez <gabriel.e.ramirez@gmail.com>
2016-06-23 22:09:48 -0400

    First commit

---
+++ cred-filter.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "os"
+    "strings"
+)
+
+//newEnvStringReplacer creates a string replacer for env variable text
+func newEnvStringReplacer() *strings.Replacer {
+	
+	var envVars []string
+	
+	for _, envVar := range os.Environ() {
+		pair := strings.Split(envVar, "=")
+		if pair[1] != "" {
+			envVars = append(envVars, pair[1])
+			envVars = append(envVars, "[redacted]")
+		}
+	}
+	
+	return strings.NewReplacer(envVars...)
+}
+
+func main() {
+    
+	envStringReplacer := newEnvStringReplacer()
+
+	scanner := bufio.NewScanner(os.Stdin)
+    for scanner.Scan() {
+        fmt.Println(envStringReplacer.Replace(scanner.Text()))
+    }
+    if err := scanner.Err(); err != nil {
+        fmt.Fprintln(os.Stderr, "error:", err)
+        os.Exit(1)
+    }
+}
+
---
+++ test.sh
@@ -0,0 +1,5 @@
+ls therealpassword 
+echo "dsdf go1.6.2 sdfads"
+echo "start NOW therealpassword"
+sleep 3
+echo "start NOW therealpassword"
